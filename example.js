405eff1f8df701499fa4d8f9d4425cfec695bbf7
Emil Malanowski <emil.malanowski@gmail.com>
2023-03-27 16:59:37 +0200

    hideActiveSlide and showSlide added app.js

---
+++ example.js
@@ -0,0 +1,24 @@
+let myFirstFunction = (name, greetings) => {
+    console.log(greetings + "! Mam na imię " + name + "!");
+}
+
+myFirstFunction("Emil", "Cześć");
+myFirstFunction("Basia", "Joł");
+
+let mnozenie = (x) => {
+    console.log(x*2);
+}
+
+mnozenie(22);
+
+let mnozenie2 = (x, y) => {
+    console.log(x*y);
+}
+
+mnozenie2(2,3);
+
+let mnozenie3 = (x) => {
+    console.log(x**2);
+}
+
+mnozenie3(6);
